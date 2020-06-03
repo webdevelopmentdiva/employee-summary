43945dac314bb5eaa9eb7726bb123af8dbd70637
Skyler Bluth <skylerbluth@Skylers-MacBook.local>
2020-06-03 00:09:24 -0700

    require statements

---
+++ Develop/lib/index.js
@@ -0,0 +1,5 @@
+const employee = require("employee");
+const inquirer = require("inquirer");
+const engineer = require("./Engineer");
+const intern = require("./Intern");
+const manager = require("./Manager");
