272900deb9497883e61e430e6ee34b6a004a7eb7
Skyler Bluth <skylerbluth@Skylers-MacBook.local>
2020-06-09 16:10:08 -0700

    got the questions running in terminal

--- Develop/app.js
+++ Develop/app.js
@@ -4,15 +4,33 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
+function createTeam() {
+  inquirer
+    .prompt([
+      {
+        name: "addMember",
+        type: "list",
+        message: "Would you like to add a team member?",
+        choices: ["Yes", "No my team is complete"],
+      },
+    ])
+    .then(function (data) {
+      switch (data.addMember) {
+        case "Yes, add to the Team":
+          renderer();
+          break;
+        case "No my team is complete":
+          buildTeam();
+          break;
+      }
+    });
+}
+createTeam();
 function renderer() {
   inquirer
     .prompt([
@@ -49,6 +67,7 @@ function renderer() {
           },
         ]);
       }
+      console.log(answers);
       if (answers === Manager) {
         prompt([
           {
@@ -58,6 +77,7 @@ function renderer() {
           },
         ]);
       }
+      console.log(answers);
       if (answers === Intern) {
         prompt([
           {
@@ -67,7 +87,6 @@ function renderer() {
           },
         ]);
       }
+      console.log(answers);
     });
 }
-
-renderer();
